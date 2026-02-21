dcb7c40291d20747ebc465800451db8e225954c9
otto616 <ottobielrodenas@gmail.com>
2026-02-21 13:40:42 +0100

    initial structure

---
+++ backend/src/main/java/com/portfolio/clinic/ClinicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicalApiApplication.class, args);
+	}
+
+}
